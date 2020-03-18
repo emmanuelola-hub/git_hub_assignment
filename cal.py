c0c7c6c2b2b8bfd9eba4d13cd5fb135796d51a57
Olayiwola <olayiwolaemma49@gmail.com>
2020-03-18 12:41:34 -0700

    gitpush

---
+++ cal.py
@@ -0,0 +1,13 @@
+Python 3.8.0 (tags/v3.8.0:fa919fd, Oct 14 2019, 19:21:23) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+=========== RESTART: C:/Users/Iyanuoluwa Olayiwola/Desktop/source.py ===========
+what's is the square root of the number
+12
+the square root of 12 is 3.4641016151377544
+>>> 
+=========== RESTART: C:/Users/Iyanuoluwa Olayiwola/Desktop/source.py ===========
+what's is the square root of the number
+16
+the square root of 16 is 4.0
+>>> 
---
+++ source.py
@@ -0,0 +1,4 @@
+print("what's is the square root of the number")
+num = input()
+square_root = int(num)**0.5
+print(f"the square root of {num} is {square_root}")
